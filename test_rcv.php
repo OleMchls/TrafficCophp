7d79893eefbb10a594aee61ce5ebeaf27407db38
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-04-07 18:54:23 +0200

    added test files for send and receive

---
+++ test_rcv.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/vendor/.composer/autoload.php';
+
+use TrafficCophp\Network\SocketTransport;
+use TrafficCophp\Publisher\Publisher;
+use TrafficCophp\Subscriber\Subscriber;
+use TrafficCophp\Message\SubscribeMessage;
+use TrafficCophp\Message\PublishMessage;
+use TrafficCophp\Channel\ChannelCollection;
+use TrafficCophp\Channel\Channel;
+use TrafficCophp\Message\ServerMessage;
+
+$_ENV['traffic_cop_host'] = '127.0.0.1';
+$_ENV['traffic_cop_port'] = 3542;
+
+$transport = new SocketTransport($_ENV['traffic_cop_host'], $_ENV['traffic_cop_port']);
+$subscriber = new Subscriber($transport);
+$channels = new ChannelCollection();
+$channels->addChannel(new Channel('channel_one'));
+$subscribeMessage = new SubscribeMessage($channels);
+$subscriber->subscribe($subscribeMessage);
+
+$subscriber->receive($message = new ServerMessage(new ChannelCollection()));
+
+var_dump($message);
---
+++ test_send.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/vendor/.composer/autoload.php';
+
+use TrafficCophp\Network\SocketTransport;
+use TrafficCophp\Publisher\Publisher;
+use TrafficCophp\Subscriber\Subscriber;
+use TrafficCophp\Message\SubscribeMessage;
+use TrafficCophp\Message\PublishMessage;
+use TrafficCophp\Channel\ChannelCollection;
+use TrafficCophp\Channel\Channel;
+use TrafficCophp\Message\ServerMessage;
+
+$_ENV['traffic_cop_host'] = '127.0.0.1';
+$_ENV['traffic_cop_port'] = 3542;
+
+$transport = new SocketTransport($_ENV['traffic_cop_host'], $_ENV['traffic_cop_port']);
+$publisher = new Publisher($transport);
+$channel = new Channel('channel_one');
+$message = new PublishMessage($channel, 'Hey there, im a message');
+$publisher->publish($message);
